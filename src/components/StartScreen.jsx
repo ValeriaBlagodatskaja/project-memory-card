b5c36e0ebe29f9af9deef8e7b054a6d56e4ce733
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-20 12:06:12 +0200

    create StartScreen component

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,26 @@
+import "./StartScreen.css";
+import { useState } from "react";
+
+const StartScreen = ({ onStartGame }) => {
+  const [showCards, setShowCards] = useState(false);
+
+  const handleStartGame = () => {
+    setShowCards(true);
+    onStartGame();
+  };
+  return (
+    <div className="start-screen-container">
+      <div className="welcome-container">
+        <h1>Welcome to the Memory Card Game</h1>
+        <p>
+          Click on cards to earn points, but do not click the same card twice!
+        </p>
+        <button className="start-button" onClick={handleStartGame}>
+          Start Game
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default StartScreen;

ad16fea826d79e6d16c38fbe65be6b035584eb1f
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-20 12:28:48 +0200

    create cardsData

---
+++ src/cardsData.jsx
@@ -0,0 +1,65 @@
+import catImage from "./assets/pussboots.webp?react";
+import donkeyImage from "./assets/donkey.png?react";
+import dragonImage from "./assets/dragon.webp?react";
+import gingerbreadImage from "./assets/gingerbread.webp?react";
+import shrekImage from "./assets/shrek.webp?react";
+import princeImage from "./assets/prince.webp?react";
+import queenImage from "./assets/queen.webp?react";
+import fionaImage from "./assets/fiona.webp?react";
+import kingImage from "./assets/king.webp?react";
+import fionaAnotherImage from "./assets/fiona-2.png?react";
+
+const cards = [
+  {
+    image: catImage,
+    imageAlt: "Cat",
+    hasBeenClicked: false,
+  },
+  {
+    image: donkeyImage,
+    imageAlt: "Donkey",
+    hasBeenClicked: false,
+  },
+  {
+    image: dragonImage,
+    imageAlt: "Dragon",
+    hasBeenClicked: false,
+  },
+  {
+    image: gingerbreadImage,
+    imageAlt: "GingerBread",
+    hasBeenClicked: false,
+  },
+  {
+    image: shrekImage,
+    imageAlt: "Shrek",
+    hasBeenClicked: false,
+  },
+  {
+    image: princeImage,
+    imageAlt: "Prince",
+    hasBeenClicked: false,
+  },
+  {
+    image: queenImage,
+    imageAlt: "Queen",
+    hasBeenClicked: false,
+  },
+  {
+    image: fionaImage,
+    imageAlt: "Fiona",
+    hasBeenClicked: false,
+  },
+  {
+    image: kingImage,
+    imageAlt: "King",
+    hasBeenClicked: false,
+  },
+  {
+    image: fionaAnotherImage,
+    imageAlt: "Fiona",
+    hasBeenClicked: false,
+  },
+];
+
+export default cards;

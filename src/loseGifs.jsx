e653c2994d885bb64cb06a3bfbd32ec90ec5913c
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-22 12:54:08 +0200

    cretae loseGifs

---
+++ src/loseGifs.jsx
@@ -0,0 +1,25 @@
+import fairyGif from "./assets/gifs/fairy.gif?react";
+import catGif from "./assets/gifs/cat.gif?react";
+import cat2Gif from "./assets/gifs/cat-2.gif?react";
+import donkeyGif from "./assets/gifs/donkey.gif?react";
+
+const loseGifs = [
+  {
+    image: fairyGif,
+    imageAlt: "Singing Fairy",
+  },
+  {
+    image: catGif,
+    imageAlt: "Laughing Cat",
+  },
+  {
+    image: cat2Gif,
+    imageAlt: "Disappointed Cat",
+  },
+  {
+    image: donkeyGif,
+    imageAlt: "Screaming Donkey",
+  },
+];
+
+export default loseGifs;

e83148ca636ce2b8b0453bc9c46dd82f52f1bccf
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-22 12:52:26 +0200

    update StartScreen component

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -11,7 +11,7 @@ const StartScreen = ({ onStartGame }) => {
   return (
     <div className="start-screen-container">
       <div className="welcome-container">
-        <h1>Welcome to the Memory Card Game</h1>
+        <h1>Welcome to Shrek&apos;s Memory Adventure! </h1>
         <p>
           Click on cards to earn points, but do not click the same card twice!
         </p>

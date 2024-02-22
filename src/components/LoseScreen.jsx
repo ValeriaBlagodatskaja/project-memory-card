5b63fef8a0068ca23a8a07b675c5021862a2196d
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-22 12:50:47 +0200

    change component name

---
+++ src/components/LoseScreen.jsx
@@ -0,0 +1,33 @@
+import "./LoseScreen.css";
+import { useState } from "react";
+import clsx from "clsx";
+import loseGifs from "../loseGifs";
+
+const LoseScreen = ({ onStartGame, className }) => {
+  const [showCards, setShowCards] = useState(false);
+
+  const handleStartGame = () => {
+    setShowCards(true);
+    onStartGame();
+  };
+
+  const randomGif = loseGifs[Math.floor(Math.random() * loseGifs.length)];
+  console.log(randomGif);
+
+  return (
+    <div className="end-screen-container">
+      <div className="end-game-container">
+        <img src={randomGif.image} className={clsx("gif", className)} />
+        <h1>Oops!</h1>
+        <p>
+          Shrek caught you clicking the same card twice. Better luck next time.
+        </p>
+        <p>Don&apos;t worry, ogres learn from their mistakes!</p>
+        <button className="start-button" onClick={handleStartGame}>
+          Play again
+        </button>
+      </div>
+    </div>
+  );
+};
+export default LoseScreen;

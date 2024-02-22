082159fc8b89743a74716861644c3df776b40e81
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-22 12:53:40 +0200

    create winGifs

---
+++ src/winGifs.jsx
@@ -0,0 +1,20 @@
+import happyDonkeyGif from "./assets/gifs/happydonkey.gif?react";
+import ogreFamilyGif from "./assets/gifs/ogrefamily.gif?react";
+import smileyShrekGif from "./assets/gifs/smile.gif?react";
+
+const winGifs = [
+  {
+    image: happyDonkeyGif,
+    imageAlt: "Singing Donkey",
+  },
+  {
+    image: ogreFamilyGif,
+    imageAlt: "Shrek's Family",
+  },
+  {
+    image: smileyShrekGif,
+    imageAlt: "Smiley Shrek",
+  },
+];
+
+export default winGifs;
